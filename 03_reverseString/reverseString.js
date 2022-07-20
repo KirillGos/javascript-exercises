5f1ce8fd92ecf3722cf7a29416788a32d4df406d
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-07-20 20:07:08 +0300

    Completed exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+//1.Split the string into the numbers
+const reverseString = function(string) {
+    let letter = [];
+    if(string != 0) {
+        for(i = 0; i < string.length; i++) {
+            letter.push(string.slice(i, i + 1)); 
+        }
+        return shift(letter);
+    } else {
+        return string;
+    }
+};
 
+const shift = array => {
+    let result  = [];
+    const arrayLength = array.length;
+    for(let i = 0; i < arrayLength; i++) {
+         result += array.pop();
+    }
+        return result
 };
 
 // Do not edit below this line
