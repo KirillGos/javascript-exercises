54f1dab6c859f0d230ea7ba79c913aaa2a8901e7
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-07-21 16:40:47 +0300

    Completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = (array, ...restArg) => {
+    let remove = [];
+    for (let j = 0; j < array.length; j++) {
+      for(let i = 0; i < restArg.length; i++) {
+       if( array[j] === restArg[i] ) {remove.push(array.indexOf(array[j]))}
+        
+      }
+    }
+ for (let a = 0; a < remove.length; a++) {
+      if (a % 2 !== 0 && a !== 0) {
+          array.splice(remove[a] -1, 1)
+          remove[a]--
+      } else {
+          array.splice(remove[a] , 1)
+          remove[a]--
+         }
+    }
+ return array;    
+}
 // Do not edit below this line
 module.exports = removeFromArray;
